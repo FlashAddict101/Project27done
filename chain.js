ba7f3678b73ccb2b8a73c0816bfc00b7c07aa621
FlashAddict101 <68386799+FlashAddict101@users.noreply.github.com>
2020-08-07 12:20:52 -0400

    Add files via upload

---
+++ chain.js
@@ -0,0 +1,18 @@
+// class chain{
+//     constructor(bodyA,bodyB){
+//         var options={
+//             bodyA:bob.body,
+//             bodyB:constrainedLog.body,
+//             stiffness:0.04,
+//             length:80
+//          } 
+//          this.chain=Constraint.create(options)
+//          World.add(world,this.chain)
+//         }
+//         display(){
+//             var pointA = this.chain.bodyA.position;
+//             var pointB = this.chain.bodyB.position; 
+//             strokeWeight(4);
+//             line(pointA.x, pointA.y, pointB.x, pointB.y);
+//         }
+// }
